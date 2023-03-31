e1383fe6b80fc3c055dfefd9f38148199792bb52
Faith-Gisemba <faithmgisemba@gmail.com>
2023-03-31 23:08:43 +0300

    functions

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer = a + b
+    return answer
+
+def substract(d,e):
+    answer = d - e
+    return answer
+
+def divide(f,g):
+    answer = f / g
+    return answer
+
+def remainder(x,y):
+    answer = x % y
+    return answer
+
+def multiply(v,p):
+    answer = v * p
+    return answer   
